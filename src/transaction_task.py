c67d0c4e09c8304828b195e34bc64eb7e1450d19
marcus-ny <kyalsin321@gmail.com>
2023-09-09 11:29:01 +0800

    physical top up

--- src/transaction_task.py
+++ src/transaction_task.py
@@ -4,8 +4,7 @@ class TransactionTask:
         self._sender_uid = sender_uid
         self._recipient_uid = recipient_uid
         if (amount <= 0):
-            # TODO: Remove in production! (Marcus)
-            print("Yall broke mfs")
+            print("Insufficient balance")
         else:
             self._amount = amount
         self._date = date
@@ -18,7 +17,7 @@ class TransactionTask:
 
     def get_amount(self) -> float:
         return self._amount
-    
+
     def get_date(self) -> str:
         return self._date
 
@@ -33,6 +32,6 @@ class TransactionTask:
 
     def set_date(self, date: str) -> None:
         self._date = date
-        
+
     def to_row(self):
         return (self._sender_uid, self._recipient_uid, self._amount, self._date)
